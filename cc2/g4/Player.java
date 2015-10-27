4a88fab0f6df86e2ff230cae398732279da4eb40
Nam Hoang <nnh2110@columbia.edu>
2015-10-26 22:26:48 -0400

    cut()

--- cc2/g4/Player.java
+++ cc2/g4/Player.java
@@ -70,20 +70,21 @@ public class Player implements cc2.sim.Player {
     }
     // find all valid cuts
     ArrayList <Move> moves = new ArrayList <Move> ();
-    for (int i = 0 ; i != dough.side() ; ++i)
-      for (int j = 0 ; j != dough.side() ; ++j) {
-        Point p = new Point(i, j);
-        for (int si = 0 ; si != shapes.length ; ++si) {
-          if (shapes[si] == null) continue;
-          Shape[] rotations = shapes[si].rotations();
-          for (int ri = 0 ; ri != rotations.length ; ++ri) {
-            Shape s = rotations[ri];
-            if (dough.cuts(s, p))
-              moves.add(new Move(si, ri, p));
+    for (int si = 0 ; si != shapes.length ; ++si) {
+      if (shapes[si] == null) continue;
+      for (int i = 0 ; i != dough.side() ; ++i) {
+        for (int j = 0 ; j != dough.side() ; ++j) {
+          Point p = new Point(i, j);
+            Shape[] rotations = shapes[si].rotations();
+            for (int ri = 0 ; ri != rotations.length ; ++ri) {
+              Shape s = rotations[ri];
+              if (dough.cuts(s, p))
+                moves.add(new Move(si, ri, p));
           }
         }
       }
+    }
     // return a cut randomly
-    return moves.get(gen.nextInt(moves.size()));
+    return moves.get(0);
   }
 }
