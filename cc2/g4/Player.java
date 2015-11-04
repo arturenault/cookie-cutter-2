797c56bed63cc1ec0d516a0b45e44c21def3e2ed
Artur Upton Renault <artur.renault@gmail.com>
2015-11-03 22:47:15 -0500

    Better piece placement and 8

--- cc2/g4/Player.java
+++ cc2/g4/Player.java
@@ -13,12 +13,15 @@ public class Player implements cc2.sim.Player {
 
   private Random gen = new Random();
 
+  private int lastLength = 0;
+
   public Shape cutter(int length, Shape[] shapes, Shape[] opponent_shapes)
   {
     Point[] cutter = new Point[length];
     /* If length is 11, build the mold we want to use for the
      * big and small shapes */
     if (length == 11) {
+      lastLength = 11;
       mold = new boolean[4][4];
       for (int i = 0; i < mold.length; i++) {
         mold[0][i] = true;
@@ -35,22 +38,82 @@ public class Player implements cc2.sim.Player {
         }
       }
 
-    /* Create just one long general shape for 8 */
+      /* Create just one long general shape for 8 */
     } else if (length == 8) {
-      int count = 0;
-      for (int i = 0; i < length; i++) {
-        cutter[count++] = new Point(i / 4, i % 4);
-      }
+      if (lastLength != 8) {
+        lastLength = 8;
 
-    /* Build opposite of 11-sized shape */ 
-    } else {
-      int count = 0;
-      for (int i = 0; i < mold.length; i++) {
-        for (int j = 0; j < mold.length; j++) {
-          if (!mold[i][j]) {
-            cutter[count++] = new Point(i,j);
+        Shape opponent11 = opponent_shapes[0];
+
+        int min_i = Integer.MAX_VALUE;
+        int min_j = Integer.MAX_VALUE;
+        int max_i = Integer.MIN_VALUE;
+        int max_j = Integer.MIN_VALUE;
+
+        Iterator<Point> iter = opponent11.iterator();
+
+        while(iter.hasNext()) {
+          Point p = iter.next();
+          if (min_i > p.i) min_i = p.i;
+          if (max_i < p.i) max_i = p.i;
+          if (min_j > p.j) min_j = p.j;
+          if (max_j < p.j) max_j = p.j;
+        }
+
+        int height = max_i - min_i + 1;
+        int width = max_j - min_j + 1;
+
+        System.out.println("height: " + height +", width: " + width);
+        boolean[][] opp = new boolean[height][width];
+
+        iter = opponent11.iterator();
+        while (iter.hasNext()) {
+          Point p = iter.next();
+          System.out.println("point i: " + p.i + ", width: " + p.j);
+          opp[p.i - min_i][p.j - min_j] = true;
+        }
+
+        int count = 0;
+        for (int threshold = 0; threshold < 4; threshold++) {
+          for (int i = 0; i < height + threshold && count < length; i++) {
+            for (int j = 0; j < width + threshold && count < length; j++) {
+              if(!opp[i][j]) {
+                cutter[count++] =  new Point(i,j);
+              }
+            }
+          }
+          for (int i = 0; i < length; i++) {
+            System.out.println("(" + cutter[i].i + ", " + cutter[i].j + ")");
+          }
+          if (Point.shape(cutter)) {
+            return new Shape(cutter);
           }
         }
+      }
+      for (int i = 0; i < length; i++) {
+        cutter[i] = new Point(i / 4, i % 4);
+      }
+      return new Shape(cutter);
+
+      /* Build opposite of 11-sized shape */ 
+    } else {
+      if (lastLength != 5) {
+        lastLength = 5;
+        int count = 0;
+        for (int i = 0; i < mold.length; i++) {
+          for (int j = 0; j < mold.length; j++) {
+            if (!mold[i][j]) {
+              cutter[count++] = new Point(i,j);
+            }
+          }
+        }
+      } else {
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+          cutter[count++] = new Point(i, 0);
+        }
+        int extra = gen.nextInt(4);
+        cutter[count] = new Point(1, extra);
       }
     }
     return new Shape(cutter);
@@ -75,11 +138,29 @@ public class Player implements cc2.sim.Player {
       for (int i = 0 ; i != dough.side() ; ++i) {
         for (int j = 0 ; j != dough.side() ; ++j) {
           Point p = new Point(i, j);
-            Shape[] rotations = shapes[si].rotations();
-            for (int ri = 0 ; ri != rotations.length ; ++ri) {
-              Shape s = rotations[ri];
-              if (dough.cuts(s, p))
-                moves.add(new Move(si, ri, p));
+          Shape[] rotations = shapes[si].rotations();
+          for (int ri = 0 ; ri != rotations.length ; ++ri) {
+            Shape s = rotations[ri];
+            if (dough.cuts(s, p)) {
+              if (s.size() == 11 && ri == 2) {
+                int latitude = i - 1;
+                if (latitude < 0) {
+                  return new Move(si, ri, p);
+                } else {
+                  boolean ideal = true;
+                  for (int longitude = p.j + 1; longitude < p.j + 3; longitude++) {  
+                    if (!dough.uncut(latitude + 1, longitude)) {
+                      ideal = false;
+                      break;
+                    }
+                  }
+                  if (ideal) {
+                    return new Move(si, ri, p);
+                  }
+                }
+              }
+              moves.add(new Move(si, ri, p));
+            }
           }
         }
       }
