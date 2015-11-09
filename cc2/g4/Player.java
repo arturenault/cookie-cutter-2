fff1215be2dba806b76392fb1590d4a118f95028
Nam Hoang <nnh2110@columbia.edu>
2015-11-08 22:42:33 -0500

    add getOppMove()

--- cc2/g4/Player.java
+++ cc2/g4/Player.java
@@ -19,6 +19,10 @@ public class Player implements cc2.sim.Player {
 
   private Queue<Shape> backup8shapes;
 
+  // aggressive cuts variables
+  ArrayList<Move> oppMoves = new ArrayList<>();
+  Dough lastDough = new Dough(50);
+
   public Shape cutter(int length, Shape[] shapes, Shape[] opponent_shapes)
   {
     if (firstRun = true) {
@@ -134,6 +138,18 @@ public class Player implements cc2.sim.Player {
 
   public Move cut(Dough dough, Shape[] shapes, Shape[] opponent_shapes)
   {
+    Move cut = realCut(dough, shapes, opponent_shapes);
+
+    // cut on our simulated dough
+    // System.out.println("Our move: ("+cut.shape+", "+cut.rotation+", "+cut.point+")");
+    lastDough.cut(shapes[cut.shape].rotations()[cut.rotation], cut.point);
+
+    return cut;
+  }
+
+  public Move realCut(Dough dough, Shape[] shapes, Shape[] opponent_shapes)
+  {
+    getOppMove(dough, opponent_shapes);
     // prune larger shapes if initial move
     if (dough.uncut()) {
       int min = Integer.MAX_VALUE;
@@ -178,7 +194,6 @@ public class Player implements cc2.sim.Player {
         }
       }
     }
-    // return a cut randomly
     return moves.get(0);
   }
 
@@ -206,4 +221,95 @@ public class Player implements cc2.sim.Player {
     shape = new Shape(points);
     backup8shapes.add(shape);
   }
+
+  private Move aggressiveCut(Dough dough, Shape[] shapes, Shape[] opponent_shapes) {
+
+    return new Move(0, 0, new Point(0,0));
+  }
+
+  private Move getOppMove(Dough dough, Shape[] opponent_shapes) {
+    int mShape;
+    int mRotation = -1;
+    Point mPoint;
+
+    ArrayList<Point> oppPoints = new ArrayList<>();
+    Shape oppShape;
+
+    // shape match between size and index
+    HashMap<Integer, Integer> shapeMatch = new HashMap<>();
+    shapeMatch.put(11, 0);
+    shapeMatch.put(8, 1);
+    shapeMatch.put(5, 2);
+
+    boolean first = true;
+    for (int x = 0; x < dough.side(); x++) {
+      for (int y = 0; y < dough.side(); y++) {
+        if (lastDough.uncut(x, y) && !dough.uncut(x, y)) {
+          oppPoints.add(new Point(x, y));
+
+          if (first) {
+            mPoint = new Point(x, y);
+            first = false;
+          }
+        }
+      }
+    }
+
+    if (oppPoints.size() == 0) {
+      System.out.println("Opp didn't cut");
+      return null;
+    }
+
+    // find mShape
+    mShape = shapeMatch.get(oppPoints.size());
+
+    // find mRotation
+    oppShape = new Shape(oppPoints.toArray(new Point[oppPoints.size()]));
+    mRotation = findRotation(opponent_shapes[mShape], oppShape);
+
+    // find mPoint
+    int min_i = Integer.MAX_VALUE;
+    int min_j = Integer.MAX_VALUE;
+    int max_i = Integer.MIN_VALUE;
+    int max_j = Integer.MIN_VALUE;
+    for (Point p : oppPoints) {
+      if (p.i < min_i) min_i = p.i;
+      if (p.i > max_i) max_i = p.i;
+      if (p.j < min_j) min_j = p.j;
+      if (p.j > max_j) max_j = p.j;
+    }
+    mPoint = new Point(min_i, min_j);
+
+    // cut on simulated dough
+    lastDough.cut(opponent_shapes[mShape].rotations()[mRotation], mPoint);
+
+    // System.out.println("Opp last move: ("+mShape+", "+mRotation+", "+mPoint+")");
+    return new Move(mShape, mRotation, mPoint);
+  }
+
+  /**
+   * Find if s2 is any rotation of s1.
+   * If yes, return the rotation index. Otherwise return -1
+   */
+  private int findRotation(Shape s1, Shape s2) {
+    Shape[] rotShapes = s1.rotations();
+    for (int i = 0; i < 4; i++) {
+      Set<Point> points1 = new HashSet<>();
+      for (Point p : rotShapes[i]) {
+        points1.add(p);
+      }
+
+      Set<Point> points2 = new HashSet<>();
+      for (Point p : s2) {
+        points2.add(p);
+      }
+
+      if (points1.equals(points2)) {
+        return i;
+      }
+    }
+
+  return -1;
+  }
+
 }
